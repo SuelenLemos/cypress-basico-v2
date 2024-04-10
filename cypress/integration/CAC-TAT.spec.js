e4da8cd969827c0c39c5fe9bda2f9438ac3f90df
suelen.santos <suelen.santos@digicade.com.br>
2024-04-10 15:05:34 -0300

    primeirotestevisitamurllocal

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,3 +4,15 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() { // titulo do teste
+    it('verifica o título da aplicação', function() { //o que o teste vai verificar
+
+
+        cy.visit( './src/index.html')//visitar uma url local
+        cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')  //confere o titulo da url
+
+    })
+  })
+  
