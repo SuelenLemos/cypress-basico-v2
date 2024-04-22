0339ef8fe23f56584bf1fd0b7c0214286d98c39b
suelen.santos <suelen.santos@digicade.com.br>
2024-04-22 15:16:37 -0300

    correcaoCodigo

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,41 +7,35 @@
 /// <reference types="Cypress" />
 
 
-beforeEach(()=> {
-
     describe('Central de Atendimento ao Cliente TAT', function() { // titulo do teste
+     beforeEach(function(){
+      cy.visit( './src/index.html')//visitar uma url local
+     })
         it('verifica o título da aplicação', function() { //o que o teste vai verificar
-    
-    
-            cy.visit( './src/index.html')//visitar uma url local
+
             cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')  //confere o titulo da url
     
         })
       })
 
 
-})
+
 
   describe('preenche os campos obrigatórios e envia o formulário', function(){
-    it('preenche os textos',function(){
+    it('preenche os textos',function(){ //se quiser testar apenas um ponto isolado , coloca it.only para ele rodar somente um determinado teste
 
-        cy.get('input[id=firstName]').click()
-        cy.get('input[id=firstName]').type('Fulano')
+        cy.get('input[id=firstName]').type('Fulano') //cypress já identifica e seleciona o campo, por este motivo não há necessidade de clicar para selecionar
         
-        cy.get('input[id=lastName]').click()
         cy.get('input[id=lastName]').type('De Tal')
 
-        cy.get('input[id=email]').click()
         cy.get('input[id=email]').type('fulano@teste.com')
 
-        cy.get('input[id=phone]').click()
         cy.get('input[id=phone]').type('99999999')
         
-        cy.get('input[id=email]').click()
-        cy.get('input[id=email]').type('fulano@teste.com')
-     
-        cy.get('select[id=product]').should('be.visible').first().click()
-        cy.get('select[option value="Selecione"]')
+         
+        cy.select('[id=product]').should('be.visible').first().click()
+        
+        
 
         
     })
