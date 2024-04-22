11792ae7184d09b381c7d6158c1b1e2fb3303bf2
suelen.santos <suelen.santos@digicade.com.br>
2024-04-22 16:40:33 -0300

    atualizacoesteste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -23,20 +23,40 @@
 
   describe('preenche os campos obrigatórios e envia o formulário', function(){
     it('preenche os textos',function(){ //se quiser testar apenas um ponto isolado , coloca it.only para ele rodar somente um determinado teste
-
+        const textolongo = 'Olá este texto é extenso,este texto é extenso,este texto é extenso,este texto é extenso,este texto é extenso,este texto é extenso,este texto é extenso,'
         cy.get('input[id=firstName]').type('Fulano') //cypress já identifica e seleciona o campo, por este motivo não há necessidade de clicar para selecionar
         
         cy.get('input[id=lastName]').type('De Tal')
+        //´podemos utilizar o comando 'input[id=lastName]' para identificarmos o tipo do campo e o id do campo 
+        //OU podemos simplesmente utilizar #iddocampo sem precisar de escrever a palavra 'id' pois o cypress já identifica que '#' é um ID                                          
 
-        cy.get('input[id=email]').type('fulano@teste.com')
-
-        cy.get('input[id=phone]').type('99999999')
-        
+        cy.get('#email').type('fulano@teste.com')
+        cy.get('#phone').type('99999999')
+        cy.get('#open-text-area').type(textolongo, {delay:0})
          
-        cy.select('[id=product]').should('be.visible').first().click()
         
-        
-
-        
+        cy.get('.button[type="submit"]').click() //estamos usando classe 'button' type 'submit' para identificar o botão de enviar, depois comando de clicar
+        cy.get('.success').should('be.visible')
+        cy.get('#product').type('cursos')        
     })
+  })
+
+    describe('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+      it('preenche os textos',function(){ //se quiser testar apenas um ponto isolado , coloca it.only para ele rodar somente um determinado teste
+          const textolongo = 'Olá este texto é extenso,este texto é extenso,este texto é extenso,este texto é extenso,este texto é extenso,este texto é extenso,este texto é extenso,'
+          cy.get('#firstName').type('Fulano') //cypress já identifica e seleciona o campo, por este motivo não há necessidade de clicar para selecionar
+          
+          cy.get('#lastName').type('De Tal')
+          //´podemos utilizar o comando 'input[id=lastName]' para identificarmos o tipo do campo e o id do campo 
+          //OU podemos simplesmente utilizar #iddocampo sem precisar de escrever a palavra 'id' pois o cypress já identifica que '#' é um ID                                          
+  
+          cy.get('#email').type('fulano.com')
+          cy.get('#phone').type('99999999')
+          cy.get('#open-text-area').type(textolongo, {delay:0})
+           
+          
+          cy.get('.button[type="submit"]').click() //estamos usando classe 'button' type 'submit' para identificar o botão de enviar, depois comando de clicar
+          cy.get('.error').should('be.visible')
+          cy.get('#product').type('cursos')        
+      })
   })
